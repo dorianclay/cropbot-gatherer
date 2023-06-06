935d99418a02200005e4956fe03bc41a52e94bd5
Dorian Clay <dorian@dorianclay.com>
2023-06-06 00:49:01 -0700

    Add nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -15,15 +15,21 @@ export default function Page({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   return (
-    <>
+    <div className="h-screen">
       <Navbar />
-      <h1 className="text-2xl font-bold underline">Hello world!</h1>
-      {/* <div className="text-3xl font-bold">
-        <h1>Cropbot Harvester 🚜</h1>
-        <Link href="/gather">
-          <button>Gather Data</button>
+      <div className="flex flex-col justify-center text-3xl font-bold h-[85vh] px-4">
+        <div className="flex justify-center text-center py-5">
+          Cropbot is an automatic image cropping tool.
+        </div>
+        <div className="flex justify-center text-center py-5">
+          Help gather data today!
+        </div>
+        <Link href="/gather" className="py-5 flex justify-center">
+          <button className="rounded-full bg-purple-400 w-52 h-12 text-2xl font-semibold hover:drop-shadow-2xl hover:scale-110 ease-in-out duration-150">
+            Gather Data
+          </button>
         </Link>
-      </div> */}
-    </>
+      </div>
+    </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [pagesVisible, setPagesVisible] = useState(false);
+  const [lock, setLock] = useState(false);
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+    setLock(true);
+    pagesVisible
+      ? setPagesVisible(!pagesVisible)
+      : setTimeout(() => {
+          setPagesVisible(!pagesVisible);
+        }, 500);
+  };
+
   return (
-    <nav className="fixed w-full h-24 shadow-xl bg-black">
-      <div></div>
-    </nav>
+    <>
+      <nav
+        className={
+          menuOpen
+            ? `fixed w-full h-48 drop-shadow-2xl bg-white hover:drop-shadow-2xl ease-in-out duration-500 landscape:h-24`
+            : "fixed w-full h-24 drop-shadow-lg bg-white hover:drop-shadow-2xl ease-in-out duration-500"
+        }
+      >
+        <div className="flex justify-between flex-col sm:flex-row items-center h-full w-full px-5 2xl:px-16">
+          <div className="flex justify-between items-center h-24 w-full">
+            <Link href="/">
+              <div className="text-3xl hover:underline bg-transparent">
+                Cropbot 🚜
+              </div>
+            </Link>
+            <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24">
+              {menuOpen ? (
+                <AiOutlineClose size={25} />
+              ) : (
+                <AiOutlineMenu size={25} />
+              )}
+            </div>
+          </div>
+          <div
+            className={
+              pagesVisible
+                ? "transition ease-in delay-700 duration-75 h-24"
+                : "hidden sm:flex ease-in delay-700 duration-75 h-full"
+            }
+          >
+            <ul className="flex flex-col sm:flex-row h-full sm:h-full justify-evenly">
+              <Link href="/gather" className="flex flex-col justify-center">
+                <li className="mx-10 lowercase hover:underline text-2xl">
+                  Gather
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="h-24"></div>
+    </>
   );
 };
 
